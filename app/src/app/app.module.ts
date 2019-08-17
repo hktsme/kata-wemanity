36d2b3ccad4ad10f0bf26a7515f84667bf6c7635
Yves <itdm.dottignies@gmail.com>
2019-08-17 15:39:26 +0200

    Add angular material lib and base configuration for

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,7 +18,8 @@ import { ContactService } from './services/contact.service';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [
     ContactService
